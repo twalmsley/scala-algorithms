9de7a4a1437956fc110fcc8c9e71358eefc853a7
Tony <tony@aosd.co.uk>
2026-03-11 11:41:24 +0000

    Added a memoized version

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,18 @@
 import scala.annotation.tailrec
+import scala.collection.mutable.HashMap
 
 def fib1(x: Int): Int = x match
   case 0 => 0
   case 1 => 1
   case _ => fib1(x - 1) + fib1(x - 2)
+
+def fib2(x: Int, cache: HashMap[Int, Int]): Int = x match
+  case 0 => 0
+  case 1 => 1
+  case _ =>
+    cache.get(x) match
+      case Some(y) => y
+      case None =>
+        val i = fib2(x - 1, cache) + fib2(x - 2, cache)
+        cache(x) = i
+        i
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,4 +1,5 @@
 // For more information on writing tests, see
+import scala.collection.mutable.HashMap
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("fib1 is correct"):
@@ -22,4 +23,34 @@ class MySuite extends munit.FunSuite {
     assertEquals(fib1(18), 2584)
     assertEquals(fib1(19), 4181)
 
+  test("fib2 is correct"):
+    assertEquals(fib2(1, HashMap()), 1)
+    assertEquals(fib2(2, HashMap()), 1)
+    assertEquals(fib2(3, HashMap()), 2)
+    assertEquals(fib2(4, HashMap()), 3)
+    assertEquals(fib2(5, HashMap()), 5)
+    assertEquals(fib2(6, HashMap()), 8)
+    assertEquals(fib2(7, HashMap()), 13)
+    assertEquals(fib2(8, HashMap()), 21)
+    assertEquals(fib2(9, HashMap()), 34)
+    assertEquals(fib2(10, HashMap()), 55)
+    assertEquals(fib2(11, HashMap()), 89)
+    assertEquals(fib2(12, HashMap()), 144)
+    assertEquals(fib2(13, HashMap()), 233)
+    assertEquals(fib2(14, HashMap()), 377)
+    assertEquals(fib2(15, HashMap()), 610)
+    assertEquals(fib2(16, HashMap()), 987)
+    assertEquals(fib2(17, HashMap()), 1597)
+    assertEquals(fib2(18, HashMap()), 2584)
+    assertEquals(fib2(19, HashMap()), 4181)
+
+  test("fib2 is faster than fib1"):
+    assert(time(n => fib1(n)) > time(n => fib2(n, HashMap())))
 }
+
+def time(f:Int => Int):Long = 
+  val start = System.nanoTime()
+  f(30)
+  val duration = System.nanoTime() - start
+  println(s"Duration: $duration")
+  duration
