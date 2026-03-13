b4cc117489e9f2cd4678a73fe9f4fe10588a5114
Tony <tony@aosd.co.uk>
2026-03-13 18:11:19 +0000

    Simplified TreeADT

--- src/main/scala/various/TreeADT.scala
+++ src/main/scala/various/TreeADT.scala
@@ -1,20 +1,14 @@
 enum Tree[A]:
   case EmptyTree()
   case Node(a: A, left: Tree[A], right: Tree[A])
-  case Leaf(a: A)
 
 import Tree.*
 
 def treeInsert[A](x: A, t: Tree[A])(implicit ord: Ordering[A]): Tree[A] =
 
   t match
-    case EmptyTree() => Leaf(x)
-    case Leaf(y)     =>
-      val compare = ord.compare(x, y)
-      if compare == -1 then Node(y, Leaf(x), EmptyTree())
-      else if compare == 1 then Node(y, EmptyTree(), Leaf(x))
-      else t
-    case Node(x, EmptyTree(), EmptyTree())               => t
+    case EmptyTree() => Node(x, EmptyTree(), EmptyTree())
+    case Node(y, left, right) if ord.compare(x, y) == 0  => t
     case Node(y, left, right) if ord.compare(x, y) == -1 =>
       Node(y, treeInsert(x, left), right)
     case Node(y, left, right) if ord.compare(x, y) == 1 =>
@@ -26,8 +20,7 @@ def treeInsert[A](x: A, t: Tree[A])(implicit ord: Ordering[A]): Tree[A] =
 def treeContains[A](x: A, t: Tree[A])(implicit ord: Ordering[A]): Boolean =
 
   t match
-    case EmptyTree() => false
-    case Leaf(y)     => ord.compare(x, y) == 0
+    case EmptyTree()                                     => false
     case Node(y, left, right) if ord.compare(x, y) == 0  => true
     case Node(y, left, right) if ord.compare(x, y) == -1 =>
       treeContains(x, left)
@@ -39,11 +32,9 @@ def treeFind[A](x: A, t: Tree[A])(implicit ord: Ordering[A]): Option[A] =
 
   t match
     case EmptyTree()                                     => None
-    case Leaf(y) if ord.compare(x, y) == 0               => Some(y)
     case Node(y, left, right) if ord.compare(x, y) == 0  => Some(y)
     case Node(y, left, right) if ord.compare(x, y) == -1 =>
       treeFind(x, left)
     case Node(y, left, right) if ord.compare(x, y) == 1 =>
       treeFind(x, right)
     case _ => None
-
--- src/test/scala/various/TreeADTTestSuite.scala
+++ src/test/scala/various/TreeADTTestSuite.scala
@@ -5,8 +5,8 @@ class TreeADTTestSuite extends munit.FunSuite:
     val tree: Tree[Int] =
       Node(
         3,
-        Node(2, Leaf(1), EmptyTree()),
-        Node(5, Leaf(4), EmptyTree())
+        Node(2, Node(1, EmptyTree(), EmptyTree()), EmptyTree()),
+        Node(5, Node(4, EmptyTree(), EmptyTree()), EmptyTree())
       )
     println(tree)
 
@@ -14,28 +14,46 @@ class TreeADTTestSuite extends munit.FunSuite:
     val tree: Tree[String] =
       Node(
         "3",
-        Node("2", Leaf("1"), EmptyTree()),
-        Node("5", Leaf("4"), EmptyTree())
+        Node("2", Node("1", EmptyTree(), EmptyTree()), EmptyTree()),
+        Node("5", Node("4", EmptyTree(), EmptyTree()), EmptyTree())
       )
     println(tree)
 
   test("Can insert elements into a Tree and find them"):
     val tree1 = treeInsert(1, EmptyTree())
-    assertEquals(tree1, Leaf(1))
+    assertEquals(tree1, Node(1, EmptyTree(), EmptyTree()))
 
     val tree2 = treeInsert(2, tree1)
-    assertEquals(tree2, Node(1, EmptyTree(), Leaf(2)))
+    assertEquals(tree2, Node(1, EmptyTree(), Node(2, EmptyTree(), EmptyTree())))
 
     val tree3 = treeInsert(0, tree2)
-    assertEquals(tree3, Node(1, Leaf(0), Leaf(2)))
+    assertEquals(
+      tree3,
+      Node(
+        1,
+        Node(0, EmptyTree(), EmptyTree()),
+        Node(2, EmptyTree(), EmptyTree())
+      )
+    )
 
     val tree4 = treeInsert(-1, tree3)
-    assertEquals(tree4, Node(1, Node(0, Leaf(-1), EmptyTree()), Leaf(2)))
+    assertEquals(
+      tree4,
+      Node(
+        1,
+        Node(0, Node(-1, EmptyTree(), EmptyTree()), EmptyTree()),
+        Node(2, EmptyTree(), EmptyTree())
+      )
+    )
 
     val tree5 = treeInsert(3, tree4)
     assertEquals(
       tree5,
-      Node(1, Node(0, Leaf(-1), EmptyTree()), Node(2, EmptyTree(), Leaf(3)))
+      Node(
+        1,
+        Node(0, Node(-1, EmptyTree(), EmptyTree()), EmptyTree()),
+        Node(2, EmptyTree(), Node(3, EmptyTree(), EmptyTree()))
+      )
     )
 
     assert(treeContains(-1, tree5))
