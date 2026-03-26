cde8dee96110dd10479043e615cee47d7aceb2af
Tony <tony@aosd.co.uk>
2026-03-26 13:49:26 +0000

    Use cats for monoids etc.

--- src/test/scala/monoid/MonoidTestSuite.scala
+++ src/test/scala/monoid/MonoidTestSuite.scala
@@ -1,16 +1,24 @@
 package monoid
 
-import SemigroupSyntax._
-import IntInstances.given
-import StringInstances.given
+import cats._
+import cats.data._
+import cats.syntax.all._
 import org.scalatest.funsuite.AnyFunSuite
 
 class MonoidTestSuite extends AnyFunSuite:
 
+  def parseInt(s: String): Option[Int] =
+    scala.util.Try(Integer.parseInt(s)).toOption
+
   test("some monoid examples"):
 
-    val x = fold(List(1, 2, 3))
-    val y = fold(List("1", "2", "3"))
+    val w = List("1", "2", "3").map(parseInt)
+  
+    val x: Int = Foldable[List].fold(List(1, 2, 3))
+    val y: String = Foldable[List].fold(List("1", "2", "3"))
 
     assert(x == 6)
-    assertResult(y, "123")
+    assert(y == "123")
+
+    val z1 = Traverse[List].traverse(List("1", "2"))(parseInt)
+    assert(z1 == Some(List(1, 2)))
