e7929450f4a848d09fc3f02b083e67bf13d5c5e9
Tony <tony@aosd.co.uk>
2026-03-27 15:54:29 +0000

    Trying to use types to constrain activity participants

--- src/main/scala/assembly/Assemblies.scala
+++ src/main/scala/assembly/Assemblies.scala
@@ -1,4 +1,12 @@
 package assembly
+import java.time.ZonedDateTime
+import java.time.Duration
+import java.util.UUID
+
+trait SpatioTemporalExtent:
+  val id: UUID
+
+case class Role(roleName: String)
 
 trait ThreadSizeM3
 
@@ -10,10 +18,52 @@ trait MaterialSteel
 
 trait HeadHex2mm
 
-trait M3SteelBolt10mmHexHead extends ThreadSizeM3, Diameter3mm, Length10mm, MaterialSteel, HeadHex2mm
+trait M3SteelBolt10mmHexHead
+    extends ThreadSizeM3,
+      Diameter3mm,
+      Length10mm,
+      MaterialSteel,
+      HeadHex2mm,
+      SpatioTemporalExtent
 
 trait AllenKey
 
-trait AllenKeyHex2mm extends HeadHex2mm, AllenKey
+trait AllenKeyHex2mm extends HeadHex2mm, AllenKey, SpatioTemporalExtent
 
-trait DriverBitHex2mm extends HeadHex2mm, AllenKey
+trait DriverBitHex2mm extends HeadHex2mm, AllenKey, SpatioTemporalExtent
+
+case class Participation(
+    beginning: ZonedDateTime,
+    ending: ZonedDateTime,
+    id: UUID,
+    kind: String,
+    participantIn: TightenActivity,
+    temporalPartOf: SpatioTemporalExtent,
+    role: Role
+) {
+  override def toString(): String = {
+    s"""
+  data:$id hqdm:memberOfKind hqdm:Participation.
+  data:$id hqdm:beginning "$beginning".
+  data:$id hqdm:ending "$ending".
+  data:$id hqdm:participantIn data:${participantIn.id}. 
+  data:$id hqdm:temporalPartOf data:${temporalPartOf.id} 
+  data:$id hqdm:role data:${role._1}
+"""
+  }
+}
+
+case class TightenActivity(
+    beginning: ZonedDateTime,
+    ending: ZonedDateTime,
+    id: UUID,
+    kindOfActivity: String
+) {
+  override def toString(): String = {
+    s"""
+  data:$id hqdm:beginning "$beginning".
+  data:$id hqdm:ending "$ending".
+  data:$id hqdm:kindOfActivity data:$kindOfActivity.
+"""
+  }
+}
--- src/test/scala/assembly/AssembliesTestSuite.scala
+++ src/test/scala/assembly/AssembliesTestSuite.scala
@@ -1,27 +1,64 @@
 package assembly
 
 import org.scalatest.funsuite.AnyFunSuite
+import java.time.ZonedDateTime
+import java.time.Duration
+import java.util.UUID
 
 class AssembliesTestSuite extends AnyFunSuite:
 
   test("test"):
 
-    type Tool = AllenKey & HeadHex2mm
+    type TighteningTool = AllenKey & HeadHex2mm & SpatioTemporalExtent
 
-    def tightenUsingTool[A <: Tool, B <: M3SteelBolt10mmHexHead](
-        key: A,
+    def tighten[A <: TighteningTool, B <: M3SteelBolt10mmHexHead](
+        tool: A,
         bolt: B
-    ): Unit = {}
+    ): (TightenActivity, Set[Participation]) = {
+      val beginning = ZonedDateTime.now()
+      Thread.sleep(Duration.ofMillis(100))
+      val ending = ZonedDateTime.now()
 
-    val bolt1: M3SteelBolt10mmHexHead = new M3SteelBolt10mmHexHead {}
-    val allenKey2mm = new AllenKeyHex2mm {}
-    val driverBit = new DriverBitHex2mm {}
+      val activity =
+        TightenActivity(beginning, ending, UUID.randomUUID(), "tighten")
+      val participants = Set(
+        Participation(
+          beginning,
+          ending,
+          UUID.randomUUID(),
+          "StateOfTighteningTool",
+          activity,
+          tool,
+          Role("TighteningTool")
+        ),
+        Participation(
+          beginning,
+          ending,
+          UUID.randomUUID(),
+          "StateOfM3SteelBolt",
+          activity,
+          bolt,
+          Role("TightenedBolt")
+        )
+      )
+      (activity, participants)
+    }
 
-    tightenUsingTool(allenKey2mm, bolt1)
-    tightenUsingTool(driverBit, bolt1)
+    val bolt1: M3SteelBolt10mmHexHead = new M3SteelBolt10mmHexHead {
+      val id = UUID.randomUUID()
+    }
+    val allenKey2mm = new AllenKeyHex2mm {
+      val id = UUID.randomUUID()
+    }
+    val driverBit = new DriverBitHex2mm {
+      val id = UUID.randomUUID()
+    }
+
+    val result = tighten(allenKey2mm, bolt1)
+    println("--------------------------------")
+    println(result)
+    println("--------------------------------")
 
     val x: HeadHex2mm = bolt1
-    val y: MaterialSteel = bolt1
-    val z: ThreadSizeM3 = bolt1
 
     assert(x == bolt1)
